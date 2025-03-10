bf57f9e3a6e8c57d6e8fd13870ca053a2cd88ca1
MHS-20 <muhamad.salman2001@gmail.com>
2025-03-10 18:04:00 +0100

    ex9 sum type expr

--- src/main/scala/u02/Hello.scala
+++ src/main/scala/u02/Hello.scala
@@ -7,7 +7,6 @@ object Hello extends App:
   def positive(x: Int): String = x match
     case x if x >= 0 => "positive"
     case _ => "negative"
-
   println(positive(0))
 
   val positivefunc: Int => String = x => x match
@@ -18,24 +17,20 @@ object Hello extends App:
 
   // NEGATIVE
   def fakeNegative(s: String => Boolean): (String => Boolean) = s => true
-
   println(fakeNegative(s => false)("hello scala"))
 
   def negative(p: String => Boolean): (String => Boolean) =
     s => !p(s)
-
   println(negative(s => true)("hello scala"))
 
   def genericNegative[X](p: X => Boolean): (X => Boolean) =
     s => !p(s)
-
   println(genericNegative(s => true)(5))
 
 
   // CURRYING
   def curriedCheck(x: Int)(y: Int)(z: Int): Boolean =
     x <= y && y == z
-
   println(curriedCheck(2)(3))
   println(curriedCheck(2)(3)(3))
 
@@ -47,12 +42,10 @@ object Hello extends App:
   // COMPOSITION
   def compose(f: Int => Int, g: Int => Int): Int => Int = n =>
     f(g(n))
-
   println(compose(_ - 1, _ * 2)(5))
 
   def genericCompose[A, B, C](f: B => C, g: A => B): A => C = x =>
     f(g(x))
-
   println(genericCompose[Int, Int, Int](_ - 1, _ * 2)(5))
   println(genericCompose[String, String, String](_ + "1", _ + "2")("hello "))
   println(genericCompose[Int, Int, String](_ + "hello scala", _ + 2)(5))
@@ -60,7 +53,6 @@ object Hello extends App:
   def composeThree[A, B, C, D](f: C => D, g: B => C, h: A => B): A => D = n =>
     // f(g(h(n)))
     genericCompose(genericCompose(f, g), h)(n)
-
   println(composeThree[Int, Int, Int, Int](_ - 1, _ * 2, _ + 2)(5))
   println(composeThree[String, String, String, String](_ + "1", _ + "2", _ * 2)("hello "))
   println(composeThree[Int, Int, String, String](_ + "world", _ + "hello ", _ * 5)(5))
@@ -86,8 +78,29 @@ object Hello extends App:
     @annotation.tailrec
     def _reverse(n: Int, acc: Int): Int = n match
       case 0 => acc
-      case _ => _reverse(n/10, acc*10 + n%10)
+      case _ => _reverse(n / 10, acc * 10 + n % 10)
     _reverse(n, 0)
   println(reverseNumber(12345))
 
 
+  // TASK 9
+  enum Expr:
+    case Literal(x: Int)
+    case Add(x: Expr, y: Expr)
+    case Multiply(x: Expr, y: Expr)
+
+  object Expr:
+    def evaluate(expr: Expr): Int = expr match
+      case Literal(x) => x
+      case Add(x, y) => evaluate(x) + evaluate(y)
+      case Multiply(x, y) => evaluate(x) * evaluate(y)
+
+    def show(expr: Expr): String = expr match
+      case Literal(x) => x + ""
+      case Add(x, y) => "(" + show(x) + " + " + show(y) + ")"
+      case Multiply(x, y) => "(" + show(x) + " * " + show(y) + ")"
+
+  import Expr.*
+  val expr = Add(Literal(3), Multiply(Literal(4), Literal(5)))
+  println("Espressione: " + show(expr))
+  println("Valore: " + evaluate(expr))
